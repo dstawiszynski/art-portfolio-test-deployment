02837a6282eaf946e9543db640e361b48f699711
dstawiszynski <hugoshelma@gmail.com>
2026-06-23 14:48:53 +0200

    deploy 0.2.1

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,19 +1,26 @@
 import { ApplicationConfig } from '@angular/core';
 import { providePortfolio } from '@xalpol12/ngx-artist-portfolio';
+import projectsData from '../assets/data/projects.json';
+import thumbnailsData from '../assets/data/thumbnails.json';
+import bioData from '../assets/data/bio.json';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     providePortfolio({
       config: {
-        contentApiUrl: 'https://raw.githubusercontent.com/dstawiszynski/artist-portfolio-data/refs/heads/main/',
         name: 'Zuzanna Bandosz',
         contact: {
           email: 'zuzia.bandosz@gmail.com',
           instagram: 'zbandosz'
         },
-        cacheExpirationTimeMs: 15 * 60 * 1000, // 15 minutes
+        siteTitle: 'Zuzanna Bandosz',
         disableLightboxZoom: true,
-        cloudinaryCloudName: 'drtnqrawh'
+        cloudinaryCloudName: 'drtnqrawh',
+        data: {
+          projects: projectsData as any,
+          thumbnails: thumbnailsData as any,
+          bio: bioData as any
+        }
       },
       provideRouting: true
     })
